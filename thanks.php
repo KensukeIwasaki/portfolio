c686150e251abc4b01cf07462d18e50784675f9a
KensukeIwasaki <68320005+KensukeIwasaki@users.noreply.github.com>
2020-07-17 16:43:08 +0900

    new

---
+++ thanks.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/e92658c180.js" crossorigin="anonymous"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/thanks.css">
+    <title>お問合せフォーム</title>
+</head>
+
+<?php ?>
+
+<body>
+    <div class="inner">
+        <div class="container">
+            <h1>お問い合わせ</h1>
+        <div class="thanks_msg">
+            <p>
+                お問い合わせありがとうございました。<br>
+                後ほど、担当者よりご連絡をさせていただきます。<br>
+            </p>
+            <img src="img/thanks.jpg">
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
